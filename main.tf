8fb4696b3ab0b91779278f6febb31df8dd26cc6b
hs.kim <hs.kim@mz.co.kr>
2023-12-19 17:33:20 +0000

    Added vpc module

--- main.tf
+++ main.tf
@@ -119,20 +119,11 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
-
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
-
-    project_id   = "ashicat-gcp"
-    network_name = "example-vpc"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+module "network_subnets" {
+  source  = "app.terraform.io/hyunsik-org/network/google//modules/subnets"
+  version = "3.4.0"
+  # insert required variables here
+    network_name = "gaurav-network"
+    project_id = var.project
+  subnets = var.subnets
 }
--- variables.tf
+++ variables.tf
@@ -50,3 +50,9 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "subnets" {
+    type =  list(map(string))
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+}
+
