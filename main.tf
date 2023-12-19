db7cea78adb37518183b2d1907185479f34c2e03
hs.kim <hs.kim@mz.co.kr>
2023-12-19 17:15:49 +0000

    Added vpc module

--- main.tf
+++ main.tf
@@ -119,3 +119,20 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
+
+    project_id   = "ashicat-gcp"
+    network_name = "example-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
