4a29d64a00fc22ecf2e2fc2faea508dd78aed29e
hs.kim <hs.kim@mz.co.kr>
2023-12-19 17:41:45 +0000

    Added vpc module

--- variables.tf
+++ variables.tf
@@ -51,8 +51,4 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
-variable "subnets" {
-    type =  list(map(string))
-  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
-}
 
